2b0e8761769cd6ae9af7bfa7687f0de5f8b938c6
Dilshod Komilov <komilovd@silkroadprofessionals.com>
2019-11-10 11:07:38 +0500

    Initial commit

---
+++ app/src/main/java/com/komapps/komplayer/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,46 @@
+package com.komapps.komplayer.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.komapps.komplayer.R;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    private static final String[] TAB_TITLES = new String[]{"Ваши музыки","Плейлисты"};
+
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return PlaceholderFragment.newInstance(position + 1);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return TAB_TITLES[position];
+    }
+
+    @Override
+    public int getCount() {
+        // Show 5 total pages.
+        return 2;
+    }
+}
